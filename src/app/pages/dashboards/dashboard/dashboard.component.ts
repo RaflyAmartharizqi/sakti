4e15e41a38e4d8a217df66a79632bfd0aabef9f4
Rafly Amartharizqi <raflyrizqi25@gmail.com>
2025-11-04 13:50:16 +0700

    Starting commit

---
+++ src/app/pages/dashboards/dashboard/dashboard.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+
+export class DashboardComponent implements OnInit {
+
+  // bread crumb items
+  breadCrumbItems!: Array<{}>;
+ 
+
+  ngOnInit(): void {
+    /**
+     * BreadCrumb
+     */
+    this.breadCrumbItems = [
+      { label: 'Dashboards' },
+      { label: 'Dashboard', active: true }
+    ];
+
+  }
+
+
+}
